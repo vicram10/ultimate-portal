d318bd64226f8099031606ec9955f2d75fc1a615
vicram10 <vicram_42@hotmail.com>
2024-02-28 05:36:43 -0300

    core: reborn up

---
+++ Themes/default/languages/UPAboutUs.spanish_es-utf8.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* @package Ultimate Portal
+* @version 1.0.0
+* @author vicram10
+* @copyright 2024
+*/
+
+global $settings, $txt, $scripturl, $boardurl, $mbname;
+
+//Titles Modules
+$txt['up_module_title'] = 'Modulo';
+$txt['up_module_about_title'] = 'Quienes Somos';
+$txt['about_date_registered'] = 'Fecha Registro';
+$txt['about_email_address'] = 'Correo';
+
+//Errors
+$txt['ultport_error_no_active'] = 'Error, el Modulo no esta activado';
+$txt['ultport_error_no_members'] = 'No hay Usuarios que pertenezcan a este Grupo.';
+
+?>
---
+++ up-blocks/bk-menu.php
@@ -0,0 +1,51 @@
+<?php
+/**
+* @package Ultimate Portal
+* @version 1.0.0
+* @author vicram10
+* @copyright 2024
+*/
+
+if (!defined('SMF'))
+	die('Hacking attempt...');
+	
+global $settings, $db_prefix, $boardurl, $context;
+global $smcFunc, $sourcedir;
+
+//Ultimate Portal use SMF Cache data... UP it's the best, only "UP", can create this feature
+if(!empty($ultimateportalSettings['up_reduce_site_overload']))
+{
+	if((cache_get_data('bk_menu', 1800)) === NULL)
+	{
+		LoadMainLinks();
+		cache_put_data('bk_menu', $context['main-links'], 1800);		
+	}else{
+		$context['main-links'] = cache_get_data('bk_menu', 1800);
+	}
+}else{
+	LoadMainLinks();
+}
+
+echo '
+	<table border="0" width="100%" cellpadding="5" cellspacing="1">';
+foreach($context['main-links'] as $main_link) 
+{
+	//Is Active?
+if ($main_link['active'])  
+{
+		echo '
+			<tr>
+				<td class="'.!empty($main_link['class']).'" align="left">
+					'. $main_link['icon'] .'&nbsp;
+					<a href="'. $main_link['url'] .'">
+						<span>'. $main_link['title'] .'</span>
+					</a>
+				</td>
+			</tr>';		
+}			
+	
+}
+echo '
+	</table>';				
+
+?>
